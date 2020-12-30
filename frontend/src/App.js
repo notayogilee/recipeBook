df15773cc58f5f2d76c33d96aa904ee5f0926ef7
Lee Castelani <notayogilee@gmail.com>
2020-12-30 18:30:00 -0500

    add landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Landing from './screens/Landing'
+import Login from './screens/Login'
+import Register from './screens/Register'
+
 const App = () => {
   return (
-    <div>
-
-      <main>
-        <Landing />
-      </main>
-
-    </div>
+    <Router>
+      <>
+        <Route exact path="/" component={Landing} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+      </>
+    </Router>
   );
 }
 
--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -1,25 +1,142 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
+import Hidden from '@material-ui/core/Hidden'
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
 import image from '../images/landing.jpg'
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Righteous'
+  },
+  palette: {
+    primary: {
+      light: '#ffdd71',
+      main: '#ffab40',
+      dark: '#c77c02',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#8d6e63',
+      dark: '#5f4339',
+      contrastText: '#fff',
+    }
+  }
+})
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    height: '100vh',
+    background: '#FCFFDB',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing("30%"),
+      height: theme.spacing("30%")
+    }
+  },
+  buttons: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing('10%'),
+      height: theme.spacing('10%')
+    }
+  },
+  link: {
+    textDecoration: 'none'
+  }
+}))
+
 const Landing = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <Container maxWidth="false">
-        <Typography component="div" style={{
-          backgroundImage: `url(${image})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '100vh',
-          width: '100vw',
-          overflow: 'hidden'
-        }} />
-      </Container>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="lg" className={classes.root}>
+          <Container className={classes.buttons}>
+            <Typography
+              component="header"
+              variant="h1"
+              color="primary"
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                margin: 'auto'
+              }}
+            >
+              Recipe Book
+              </Typography>
+          </Container>
+
+          <Hidden smDown>
+            <Paper
+              elevation={5}
+              component="div"
+              style={{
+                backgroundImage: `url(${image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: 'auto',
+                width: '50%',
+                overflow: 'hidden'
+              }}
+            />
+          </Hidden>
+          <Hidden xsDown>
+            <Paper
+              elevation={4}
+              component="div"
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '1rem',
+                background: '#ffab40'
+              }}
+            >
+              <Typography variant="h1" color="secondary">
+                Share
+              </Typography>
+              <Typography variant="h1" color="secondary">
+                Some
+              </Typography>
+              <Typography variant="h1" color="secondary">
+                Magic
+              </Typography>
+
+            </Paper>
+          </Hidden>
+
+          <Container className={classes.buttons} >
+            <Link to="/register" className={classes.link}>
+              <Button size="large" variant="contained" color="secondary"  >
+                Register
+            </Button>
+            </Link>
+            <Link to="/login" className={classes.link}>
+              <Button size="large" variant="contained" color="secondary" >
+                Login
+            </Button>
+            </Link>
+          </Container>
+
+        </Container>
+      </ThemeProvider>
+    </>
   )
 }
 
 export default Landing
-
-
